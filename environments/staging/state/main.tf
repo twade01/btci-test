79fdc5270d89bf679d33f7606cff18662f35810c
Unknown <thomas.wade@crownpeak.com>
2019-07-03 15:59:54 -0600

    s3 update

--- environments/staging/state/main.tf
+++ environments/staging/state/main.tf
@@ -1,4 +1,6 @@
-resource "aws_s3_bucket" "state-file-bucket" {
+# resource "aws_s3_bucket" "state-file-bucket" {
+resource "aws_s3_bucket" "terrastate-stage" {
+
   bucket = "${var.environment}-state-file"
 
   versioning {
